a22d8bf351f6c30fe5ee913ae4d3981a1112e343
JuanDluna27 <delunadelasernajuanpablo@gmail.com>
2021-10-08 23:57:00 -0500

    switch case

--- index.js
+++ index.js
@@ -9,23 +9,35 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 const TOKEN = process.env.TOKEN;
+const BOTCHANNEL = process.env.BOTCHANNEL;
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
   console.log("Ready! 🤖");
 });
-const MESSAGE_RESPONSE = ["Mundo🌎","Como estás?", "Que tal" ];
+const MESSAGE_RESPONSE = process.env.MESSAGE_RESPONSE;
+const MESSAGE_RESPONSE2 = process.env.MESSAGE_RESPONSE2;
 function gotMessage(message) {
   /* This function handler all new messages
       include the messages created by the bot
       be carefull of who & what replay
   */
-    if (
-      message.content === "Hola" || "hola"){
+    switch(message){
+      case 1:{
+        message.content == "hola";
         let randomIndex = Math.floor(Math.random() * MESSAGE_RESPONSE.length);
         message.channel.send(MESSAGE_RESPONSE[randomIndex]);
+        break;}
+      case 2:{
+        message.content == "verga";
+        let randomIndex2 = Math.floor(Math.random() * MESSAGE_RESPONSE2.length);
+        message.channel.send(MESSAGE_RESPONSE2[randomIndex2])
+        break;}
+      default:{
+        message.content.BOTCHANNEL;
+        message.channel.send("Hable bien");
+        break;}
     }
-      
   }
 client.on("messageCreate", gotMessage);
 // Login to Discord with your client's token
